99bd78a717447d4daf2f9b1cbabb08e15a6e3946
samutamm <samutamm@gmail.com>
2016-11-04 18:50:06 +0200

    Mainlayout

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,1 +1,7 @@
-console.log("ES2015 -jee");
+import React from 'react';
+import {render} from 'react-dom';
+import MainLayout from './components/layouts/main-layout.jsx';
+
+render(
+  <MainLayout />, document.getElementById('app')
+);
